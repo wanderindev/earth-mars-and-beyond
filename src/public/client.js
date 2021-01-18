ed0916352ceeae39bc76e3e1ee4a729353febb3f
Javier Feliu <javier@wanderin.dev>
2021-01-18 18:34:12 -0500

    refactor: Move setListeners to its own module

--- src/public/client.js
+++ src/public/client.js
@@ -1,4 +1,5 @@
-import {NavBar, TabPanel, Footer} from './components.js';
+import {Footer, NavBar, TabPanel} from './components.js';
+import {setListeners} from "./listeners";
 import {store} from './store.js'
 
 const root = document.getElementById('root');
@@ -56,58 +57,6 @@ window.addEventListener('load', () => {
 });
 
 
-/**
- * @description Attaches event listeners to various DOM elements.  This function is called in the render() function.
- */
-const setListeners = () => {
-    // Adds event listerners after the page is rendered
-    setTimeout(() => {
-        // Gets reference to DOM elements for event listeners
-        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-        const $tabItems = Array.prototype.slice.call(document.querySelectorAll('.tab-item'), 0);
-        const $calendars = bulmaCalendar.attach('[type="date"]', {});
-        const $apodCalendar = document.querySelector('#apod-calendar');
 
-        // Adds event listener for navigation burgers
-        if ($navbarBurgers.length > 0) {
-            $navbarBurgers.forEach(el => {
-                el.addEventListener('click', () => {
-                    const target = el.dataset.target;
-                    const $target = document.getElementById(target);
-
-                    el.classList.toggle('is-active');
-                    $target.classList.toggle('is-active');
-                });
-            });
-        }
-
-        // Adds event listener for tabs
-        if ($tabItems.length > 0) {
-            $tabItems.forEach(el => {
-                el.addEventListener('click', () => {
-                    const target = el.dataset.target;
-
-                    updateStore(store, {tabs: {active: target}});
-                });
-            });
-        }
-
-        // Initializes all Bulma calendars
-        $calendars.forEach(calendar => {
-            // Add listener to select event
-            calendar.on('select', date => {
-                console.log(date);
-            });
-        });
-
-        // Accesses the APOD calendar
-        if ($apodCalendar) {
-            $apodCalendar.bulmaCalendar.on('select', datepicker => {
-                console.log(datepicker.data.value());
-            });
-        }
-
-    }, 1000);
-}
 
 export {updateStore};
---
+++ src/public/listeners.js
@@ -0,0 +1,58 @@
+import {store} from "./store";
+import {updateStore} from "./client";
+
+/**
+ * @description Attaches event listeners to various DOM elements.  This function is called in the render() function.
+ */
+const setListeners = () => {
+    // Adds event listerners after the page is rendered
+    setTimeout(() => {
+        // Gets reference to DOM elements for event listeners
+        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+        const $tabItems = Array.prototype.slice.call(document.querySelectorAll('.tab-item'), 0);
+        //const $calendars = bulmaCalendar.attach('[type="date"]', {});
+        //const $apodCalendar = document.querySelector('#apod-calendar');
+
+        // Adds event listener for navigation burgers
+        if ($navbarBurgers.length > 0) {
+            $navbarBurgers.forEach(el => {
+                el.addEventListener('click', () => {
+                    const target = el.dataset.target;
+                    const $target = document.getElementById(target);
+
+                    el.classList.toggle('is-active');
+                    $target.classList.toggle('is-active');
+                });
+            });
+        }
+
+        // Adds event listener for tabs
+        if ($tabItems.length > 0) {
+            $tabItems.forEach(el => {
+                el.addEventListener('click', () => {
+                    const target = el.dataset.target;
+
+                    updateStore(store, {tabs: {active: target}});
+                });
+            });
+        }
+/*
+        // Initializes all Bulma calendars
+        $calendars.forEach(calendar => {
+            // Add listener to select event
+            calendar.on('select', date => {
+                console.log(date);
+            });
+        });
+
+        // Accesses the APOD calendar
+        if ($apodCalendar) {
+            $apodCalendar.bulmaCalendar.on('select', datepicker => {
+                console.log(datepicker.data.value());
+            });
+        }
+*/
+    }, 1000);
+}
+
+export {setListeners};
