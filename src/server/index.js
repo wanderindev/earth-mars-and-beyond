2c1366c28d35eeda205493ab3ecab9045bec0b3d
Javier Feliu <javier@wanderin.dev>
2021-01-19 22:44:17 -0500

    Add endpoint for retrieving a range of images

--- src/server/index.js
+++ src/server/index.js
@@ -9,16 +9,28 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 app.use('/', express.static(path.join(path.resolve(), 'src/public')));
 
-// example API call
+
+// Connect to NASA's APOD API and retrieves the image for a specific date
 app.get('/apod/:date', async (req, res) => {
-    const { date } = req.params;
+    const {date} = req.params;
     try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}&date=${date}`)
+        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}&start_date=${date}`)
             .then(res => res.json())
-        res.send({ image })
+        res.send(image)
+    } catch (err) {
+        console.log('error:', err);
+    }
+});
+
+// Connect to NASA's APOD API and retrieves the images for a date range
+app.get('/apod/:start_date/:end_date', async (req, res) => {
+    const {start_date, end_date} = req.params;
+    try {
+        let images = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}&start_date=${start_date}&end_date=${end_date}`)
+            .then(res => res.json())
+        res.send(images);
     } catch (err) {
         console.log('error:', err);
     }
