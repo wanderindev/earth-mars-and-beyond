f6048b67df9b9c4c1e26e793378d192f2df31cc7
Javier Feliu <javier@wanderin.dev>
2021-01-19 22:47:54 -0500

    feat: Add request for image ranges from APOD

--- src/public/api-calls.js
+++ src/public/api-calls.js
@@ -4,9 +4,19 @@
  * @return {object} response - An object with the APOD image information
  */
 const getImageOfTheDay = (date) => {
-    return fetch(`http://localhost:3000/apod/` + date)
+    return fetch(`http://localhost:3000/apod/${date}`)
         .then(res => res.json())
-        .then(json => console.log(json));
 }
 
-export {getImageOfTheDay};
+/**
+ * @description Gets APOD images for a date range from the backend
+ * @param {string} startDate - A string representing a date in the format YYYY-MM-DD
+ * @param {string} endDate - A string representing a date in the format YYYY-MM-DD
+ * @return {object} response - An object with an array of APOD images information
+ */
+const getImagesForDateRange = (startDate, endDate) => {
+    return fetch(`http://localhost:3000/apod/${startDate}/${endDate}`)
+        .then(res => res.json())
+}
+
+export {getImageOfTheDay, getImagesForDateRange};
