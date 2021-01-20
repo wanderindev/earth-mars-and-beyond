ec656d6c75e0d9868c80f2d4f8c5ceef1ddc2298
Javier Feliu <javier@wanderin.dev>
2021-01-19 22:50:03 -0500

    feat: Add updateApodBlockedDates function

--- src/public/utils.js
+++ src/public/utils.js
@@ -1,11 +1,39 @@
+import {getImagesForDateRange} from './api-calls.js'
+import {store} from "./store.js";
+import {updateStore} from "./client.js";
+
+
 /**
  * @description Returns a string representing a date in format YYYY-MM-DD
  * @param {Date} date - A Date object
  * @return {string} date - A string representing a date
  */
 const apodFormatDate = (date) => {
-    date = new Date(2021,0,4);
-    return date.getFullYear() + '-' + String(date.getMonth() + 1).padStart(2, '0') + '-' + String(date.getDate()).padStart(2, '0')
+    return date.getFullYear() + '-' + String(date.getMonth() + 1).padStart(2, '0') + '-' + String(date.getDate()).padStart(2, '0');
 }
 
-export {apodFormatDate};
+/**
+ * @description Updates the blockedDates array for the APOD API
+ * @param {object} apod - An object with information for the APOD API
+ * @return {object} store - The updated store object
+ */
+const updateApodBlockedDates = (apod) => {
+    const startDate = apod.checkedUntil;
+    const endDate = apod.today;
+
+    getImagesForDateRange(startDate, endDate).then(images => {
+        const newBlockedDates = images.filter(image => image.media_type === 'image').map(image => image.date);
+
+        if (newBlockedDates.length > 0) {
+            const updatedDates = [...apod.blockedDates, ...newBlockedDates];
+            const newApod = Object.assign(apod, {blockedDates: updatedDates, checkedUntil: apod.today});
+
+            return updateStore(store, {
+                apod: newApod
+            });
+        }
+    });
+
+}
+
+export {apodFormatDate, updateApodBlockedDates};
