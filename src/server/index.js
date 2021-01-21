01800e7acf49e69a2deea34c7f1828b7fa4c91ab
Javier Feliu <javier@wanderin.dev>
2021-01-20 21:26:18 -0500

    feat: Update endpoints

--- src/server/index.js
+++ src/server/index.js
@@ -7,29 +7,35 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+const apodEndpoint = 'https://api.nasa.gov/planetary/apod?api_key=' + process.env.API_KEY;
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/', express.static(path.join(path.resolve(), 'src/public')));
 
 
-// Connect to NASA's APOD API and retrieves the image for a specific date
-app.get('/apod/:date', async (req, res) => {
-    const {date} = req.params;
+// Connects to NASA's APOD API and retrieves the image for a specific date
+app.get('/apod/get_image', async (req, res) => {
+    const {date} = req.query;
+
     try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}&start_date=${date}`)
+        let image = await fetch(`${apodEndpoint}&start_date=${date}`)
             .then(res => res.json())
+
         res.send(image)
     } catch (err) {
         console.log('error:', err);
     }
 });
 
-// Connect to NASA's APOD API and retrieves the images for a date range
-app.get('/apod/:start_date/:end_date', async (req, res) => {
-    const {start_date, end_date} = req.params;
+// Connects to NASA's APOD API and retrieves the images for a date range
+app.get('/apod/get_images', async (req, res) => {
+    const {start_date, end_date} = req.query;
+
     try {
-        let images = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}&start_date=${start_date}&end_date=${end_date}`)
+        let images = await fetch(`${apodEndpoint}&start_date=${start_date}&end_date=${end_date}`)
             .then(res => res.json())
+
         res.send(images);
     } catch (err) {
         console.log('error:', err);
