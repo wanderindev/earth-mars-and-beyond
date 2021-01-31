ee9416d9db70a7418dc016ef43772d55c7333d79
Javier Feliu <javier@wanderin.dev>
2021-01-30 19:50:11 -0500

    feat: Add About page

--- src/public/components.js
+++ src/public/components.js
@@ -97,9 +97,7 @@ const EarthPage = (state) => {
       .reduce((acc, slide) => acc + slide, "");
 
     return `
-            <div id="earth" class="${
-              state.menu.active === "earth" ? "is-active" : ""
-            }">
+            <div id="earth">
                 <div class="columns">
                     <div class="column has-text-centered">
                         <h1 class="title is-size-1-desktop is-size-2-mobile">Planet Earth <br>
@@ -237,9 +235,7 @@ const BeyondPage = (state) => {
     return ``;
   } else {
     return `
-            <div id="beyond" class="${
-              state.menu.active === "beyond" ? "is-active" : ""
-            }">
+            <div id="beyond">
                 <div class="columns">
                     <div class="column has-text-centered">
                         <h1 class="title is-size-1-desktop is-size-2-mobile">
@@ -287,7 +283,78 @@ const BeyondPage = (state) => {
  * @return {string} html - The HTML for the AboutPage
  */
 const AboutPage = (state) => {
-  return ``;
+  return `
+            <div id="about">
+                <div class="columns">
+                    <div class="column has-text-centered">
+                        <h1 class="title is-size-1-desktop is-size-2-mobile">
+                          About Earth, Mars, and Beyond...<br>
+                        </h1>
+                    </div>
+                </div>
+                <div class="columns">
+                    <div class="column">
+                        <div class="block about-exp has-text-justified">
+                            <p>
+                            This website is my second project submission for the <em>Udacity Intermediate 
+                            JavaScript Nanodegree</em>.  In this project, I applied the lessons learned in 
+                            the <em>Functional Programming in JavaScript</em> course.                            
+                            </p>
+                            <h2 class="title is-size-3-desktop is-size-4-mobile">
+                              The Big Picture
+                            </h2>
+                            <p>
+                            For this project, I had to create a website that consumes from various 
+                            NASA APIs.  The goal is to apply the concepts and practices learned in 
+                            the functional programming course, such as:
+                            </p>
+                            <ul>
+                                <li>Using pure functions</li>
+                                <li>Supporting immutability with Immutable JS</li>
+                                <li>Iterating over, reshaping, and accessing information from complex API responses</li>
+                                <li>Using ES6 constructs like <em>const</em>, <em>let</em>, <em>arrow functions</em>, 
+                                and <em>the new Array methods</em></li>
+                            </ul>
+                            <h2 class="title is-size-3-desktop is-size-4-mobile">
+                              The APIs
+                            </h2>
+                            <ul>
+                                <li>
+                                  The <em>Earth</em> section consumes from NASA's EPIC API.  EPIC stands for <em>Earth 
+                                  Polychromatic Imaging Camera</em>.  The camera is onboard a deep-space weather satellite 
+                                  located roughly one million miles from Earth.
+                                </li>
+                                <li>
+                                  The <em>Mars</em> section consumes from NASA's Mars Rover Photos API.  We can 
+                                  retrieve photos sent by the <em>Spirit</em>, <em>Opportunity</em>, and <em>Curiosity</em> 
+                                  rovers from the Red Planet from this API, as well as general information about each 
+                                  rover's mission.
+                                </li>
+                                <li>
+                                  The <em>Beyond</em> section consumes from NASA's APOD API.  APOD stands for 
+                                  <em>Astronomy Picture of the Day</em>.  In this section, you will find beautiful 
+                                  pictures from the cosmos and exciting information about them.
+                                </li>
+                            </ul>
+                            <h2 class="title is-size-3-desktop is-size-4-mobile">
+                              Useful Links
+                            </h2>
+                            <p>
+                            <ul>
+                                <li>You can find my code in <a href="https://github.com/wanderindev/earth-mars-and-beyond" 
+                                target="_blank">this GitHub repository</a></li>
+                                <li>For the starter code from Udacity, visit <a href="https://github.com/udacity/nd032-c2-functional-programming-with-javascript-starter/tree/project" 
+                                target="_blank">this repository</a></li>
+                                <li>The nanodegree home page is <a href="https://www.udacity.com/course/intermediate-javascript-nanodegree--nd032" 
+                                target="_blank">here</a></li>
+                                <li>Find more information about these and other NASA APIs <a href="https://api.nasa.gov/" 
+                                target="_blank">here</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
 };
 
 /**
